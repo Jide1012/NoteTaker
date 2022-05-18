99e2eb60bc51c1137a0bdd22ea10b958388aa354
Jide Adesanya <Jide1012@yahoo.com>
2022-05-18 18:18:44 -0500

    Added routes and functionality to the js files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const notes = require('../db/db.json');
+const createNewNote = require('../lib/notes.js');
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote)
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const fs = require("fs");
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({extended:true}))
+
+//Make files static
+app.use(express.static('public'));
+
+//
+app.use(express.json());
+app.use("/api",apiRoutes);
+app.use("/",htmlRoutes);
+
+
+app.listen(PORT, () =>{
+    console.log(`API server now on port ${PORT}`);
+}
+
+)
